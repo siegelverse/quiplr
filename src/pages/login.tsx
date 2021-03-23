e00adc514450ea436a1353b437b7f4d5a52a8134
siegelverse <msiegel37@gmail.com>
2021-03-23 14:03:57 -0500

    welcome page w/styling

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -36,7 +36,7 @@ export default function Login() {
 
     return (
         <div className={styles.login_container}>
-            <img src={'../styles/assets/quipler-light.svg'} alt='logo' style={{width: '50px'}} className={styles.logo}/>
+            <img src={'../../quipler-light.svg'} alt='logo' style={{width: '50px'}} className={styles.logo}/>
             <h3 className={styles.message}>Log in to quipler</h3>
             <Formik 
             initialValues={initialValues} 
--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/welcome.module.css';
+import { GiMicrophone } from 'react-icons/gi';
+import { IoMdShareAlt } from 'react-icons/io';
+import { FaRegLaughSquint } from 'react-icons/fa';
 
 export default function WelcomePage() {
     return (
-        <div>
-            welcome!
+        <div className={styles.main}>
+            <div className={styles.wrapper}>
+                <div className={styles.left}>
+                    <div className={styles.items_wrapper}>
+                        <div className={styles.item}>
+                            <span className={styles.icon}>
+                                <GiMicrophone />
+                            </span>
+                            <span className={styles.label}>Tell Jokes.</span>
+                        </div>
+                    <div className={styles.item}>
+                        <span className={styles.icon}>
+                            <IoMdShareAlt />
+                        </span>
+                        <span className={styles.label}>Share Jokes.</span>
+                    </div>
+                    <div className={styles.item}>
+                        <span className={styles.icon}>
+                            <FaRegLaughSquint />
+                        </span>
+                        <span className={styles.label}>Laugh.</span>
+                    </div>
+                </div>
+            </div>
+    
+            <div className={styles.center}>
+                <img src={'../../quipler-light.svg'} alt="logo" style={{ width: "100px" }} />
+                <h1>
+                    See what's happening in
+                <br />
+                    the world right now
+                </h1>
+                <span>Join quiplr Today.</span>
+                <Link href='/signup'>
+                    <a className={styles.btn_sign_up}>Sign up</a>
+                </Link>
+                <Link href='/login'>
+                    <a className={styles.btn_login}>Log in</a>
+                </Link>
+            </div>
+            </div>
         </div>
     )
 }
